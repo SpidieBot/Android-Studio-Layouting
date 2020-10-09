f5a8594d84d0122d509fd4c50542b469dba7e924
SpidieBot <56400921+SpidieBot@users.noreply.github.com>
2020-10-09 22:40:51 +0700

    Final

---
+++ John_Andray_William_Doney_2201812570/Linear Layout/app/src/main/java/com/example/linarlayout/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.linarlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void Next(View view)  {
+        CountTotal();
+    }
+
+    private void CountTotal() {
+        Intent i_1 = new Intent(this, Result.class);
+
+        EditText editText1 = findViewById(R.id.number1);
+        String temp1 = editText1.getText().toString();
+        int value1=0;
+        if (!temp1.equals("")){
+            value1 = Integer.parseInt(temp1);
+        }
+        EditText editText2 = findViewById(R.id.number2);
+        String temp2 = editText2.getText().toString();
+        int value2=0;
+        if (!temp2.equals("")){
+            value2 = Integer.parseInt(temp2);
+        }
+        Spinner spinner = (Spinner) findViewById(R.id.equation);
+        String equation = spinner.getSelectedItem().toString();
+
+        i_1.putExtra("number1", value1);
+        i_1.putExtra("number2", value2);
+        i_1.putExtra("equation", equation);
+
+        startActivity(i_1);
+    }
+}
---
+++ John_Andray_William_Doney_2201812570/Relative Layout/app/src/main/java/com/example/relativelayout/Result.java
@@ -0,0 +1,47 @@
+package com.example.relativelayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Result extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        TextView txtOutput = (TextView) findViewById(R.id.result);
+
+        Intent intent = getIntent();
+        float number1 = intent.getIntExtra("number1", 0);
+        float number2 = intent.getIntExtra("number2", 0);
+        String equation = intent.getStringExtra("equation");
+        float value = 0;
+        if (equation.equals("+")){
+            value = (number1 + number2);
+        }
+        else if (equation.equals("-")){
+            value = (number1 - number2);
+        }
+        else if (equation.equals("*")){
+            value = (number1 * number2);
+        }
+        else if (equation.equals("/")){
+            value = (number1 / number2);
+        }
+        String numberAsString = Float.toString(value);
+        txtOutput.setText(numberAsString);
+
+
+
+    }
+
+    public void Back(View view) {
+        Intent i_1 = new Intent(this, MainActivity.class);
+        startActivity(i_1);
+    }
+}
